0b3e66c357a64a7978fb6bff81e9a0b422919c77
qwertyNippon <orlandohenry100@gmail.com>
2023-06-19 01:18:23 -0400

    fixed register bug

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -2,23 +2,22 @@ from flask import Blueprint, render_template, request, redirect, url_for
 from .forms import RegisterForm
 from ..models import User
 
-
 auth = Blueprint('auth', __name__, template_folder='auth_templates')
-# auth = Blueprint('auth', __name__, template_folder='auth_templates', url_prefix='auth')
 
-@auth.route('/register', methods=['GET', 'Post'])
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    print(request.method)
     if request.method == 'POST':
         if form.validate():
             username = form.username.data
             email = form.email.data
             password = form.password.data
+            print(username, email, password)
 
             user = User(username, email, password)
             user.save_user()
             return redirect(url_for('auth.login'))
-
 
     return render_template('register.html', form=form)
 
---
+++ run.py
@@ -0,0 +1,20 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def land():
+#     return {
+#         "test":"test1"
+#     }
+# @app.route('/page2')
+# def page2():
+#     return {
+#         "test":"test1"
+#     }
+# @app.route('/page3')
+# def page3():
+#     return {
+#         "test":"test1"
+#     }
+# app.run()
