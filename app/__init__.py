16bba1d0798545eee9f9f527b268339af3dd0367
qwertyNippon <orlandohenry100@gmail.com>
2023-06-20 23:14:55 -0400

    flask login

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 
+
 from config import Config
 
 from .auth.routes import auth
-from .models import db
+from .models import db, User
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 app.config.from_object(Config)
+
+login = LoginManager()
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 
 db.init_app(app)
 migrate = Migrate(app, db)
