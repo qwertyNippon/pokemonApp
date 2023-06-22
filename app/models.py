133d5afba19dc9909d294d39fa315b736ac34c1b
qwertyNippon <orlandohenry100@gmail.com>
2023-06-22 18:46:59 -0400

    changed the pics. reverted amount of pokemon code

--- app/models.py
+++ app/models.py
@@ -34,4 +34,26 @@ class Post(db.Model):
         self.img_url = img_url
         self.user_id = user_id
 
-# class Pokemon():
+class Pokemon():
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    abilities = db.Column(db.String, nullable=False)
+    base_experience = db.Column(db.Integer, nullable=False)
+    attack = db.Column(db.Integer, nullable=False)
+    defense = db.Column(db.Integer, nullable=False)
+    special_attack = db.Column(db.String, nullable=False)
+    sprites = db.Column(db.String, nullable=False)
+
+    def __init__(self, name, abilities, base_experience, attack, defense, special_attack, sprites):
+        self.name = name
+        self.abilities = abilities
+        self.base_experience = base_experience
+        self.attack = attack
+        self.defense = defense
+        self.special_attack = special_attack
+        self.sprites = sprites
+
+    def savePoke(self):
+        db.session.add(self)
+        db.session.commit()
+
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 import requests
 from .forms import PokeForm
-from flask import render_template, request
+from .models import Pokemon
+from flask import render_template, request, flash
 
 @app.route('/', methods=['GET', 'POST'])
 def land():
@@ -27,8 +28,29 @@ def land():
 
             poke_dict = func(pokeName)
             return render_template('index.html', form=form, poke_dict=poke_dict)
-    # tkinter.messagebox.showerror(title=None, message='Check spelling please', **options)
     return render_template('index.html', form=form)
+    #     if form.validate():
+    #         def func(pokeName):
+    #             data_dict = {}
+    #             res1 = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokeName}')
+    #             data = res1.json()
+    #             data_dict['name'] = data['name']
+    #             data_dict['abilities'] = data['abilities'][0]['ability']
+    #             data_dict['base_experience'] = data['base_experience']
+    #             data_dict['attack'] = data['stats'][0]
+    #             data_dict['attack'] = data['stats'][1]
+    #             data_dict['defense'] = data['stats'][2]
+    #             data_dict['special-attack'] = data['stats'][3]
+    #             data_dict['sprites'] = data['sprites']['front_shiny']
+    #             return data_dict
+    #         pokeName = form.pokemon.data
+    #         p=Pokemon.query.filter_by(name=pokeName).first()
+    #         if not p:
+    #             poke_dict = func(pokeName)
+    #             if not poke_dict:
+    #                 flash('Check your spelling')
+    #     return render_template('index.html', form=form, poke_dict=poke_dict)
+    # return render_template('index.html', form=form)
 
 
 
